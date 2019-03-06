98aeb3d0fee55e7259c0f511f5f77864ad1ecb28
iTelaSoft-User <rasitha.ariyarathna@itelasoft.com>
2019-03-06 13:12:13 +0530

    setup production webpack-build

--- src/app.js
+++ src/app.js
@@ -38,6 +38,6 @@ const store = configureStore;
 // const state = store.getState();
 // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 ReactDOM.render(<Provider store={store}>{AppRouter}</Provider>, document.getElementById("app"));
