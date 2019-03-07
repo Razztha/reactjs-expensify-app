eac95492e685ac210978d9630a978db4167ab9da
iTelaSoft-User <rasitha.ariyarathna@itelasoft.com>
2019-03-07 11:50:48 +0530

    port change to 3000

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 var express = require('express');
 var app = express();
 var publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
