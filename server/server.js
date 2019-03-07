4ae83ddfd481411f2a6b0f7f86f37be55eb17ce2
iTelaSoft-User <rasitha.ariyarathna@itelasoft.com>
2019-03-07 15:13:06 +0530

    testing 12

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
     console.log("Server is up!");
 });
