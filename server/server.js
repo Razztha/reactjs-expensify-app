fcfb159f53e0bffffa917a5050cc633fec014c00
iTelaSoft-User <rasitha.ariyarathna@itelasoft.com>
2019-03-07 11:54:35 +0530

    port name change again

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 var express = require('express');
 var app = express();
 var publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 app.use(express.static(publicPath));
 
