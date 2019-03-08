73e7e3de33eccf404aefe926705417ad1fce3fb2
iTelaSoft-User <rasitha.ariyarathna@itelasoft.com>
2019-03-08 15:41:01 +0530

    testing 19

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@
 const path = require('path');
 var express = require('express');
 var app = express();
-// var publicPath = path.join(__dirname, '..', 'Public');
-var publicPath = path.join('..', 'Public');
+var publicPath = path.join(__dirname, '..', 'Public');
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
